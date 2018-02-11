d566cf464aaab04eb162d98def6769d143736d48
Kamau Cusaac <kcusaac@gmail.com>
2018-02-10 22:30:29 -0500

    Fist Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Your first Vue file'
+    }
+  },
+  methods:{
+    greeting:function(){
+      return "hey"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
